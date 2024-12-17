ec776151820106d3a874e830b240bb72b0a6b528
ishubham-nagarro <shubham.bansal02@nagarro.com>
2024-12-17 15:18:11 +0530

    Fix User Dashboard

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -11,7 +11,7 @@ const UserDashboard = () => {
 
     useEffect(() => {
         fetchWeatherData(city);
-    });
+    }, []);
 
     const fetchWeatherData = async (city) => {
         setLoading(true);
